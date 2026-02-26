f83a4727eaa8a28aeb7b7ea53521bad9d16be784
chLi <565353780@qq.com>
2026-02-26 17:52:53 +0800

    fix setup bugs

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 cd ..
 git clone --depth 1 --recursive -b v2.8.3 https://github.com/Dao-AILab/flash-attention.git
-git clone --depth 1 https://github.com/NVlabs/nvdiffrast.git
-git clone --depth 1 --recurse-submodules https://github.com/hbb1/diffoctreerast.git
+git clone --depth 1 --recursive https://github.com/NVlabs/nvdiffrast.git
+git clone --depth 1 --recursive https://github.com/hbb1/diffoctreerast.git
 git clone --depth 1 https://github.com/autonomousvision/mip-splatting.git
 git clone --depth 1 https://github.com/facebookresearch/pytorch3d.git
 
@@ -21,7 +21,7 @@ pip install gradio==4.44.1 gradio_litmodel3d==0.0.1
 
 CC="cc -mavx2" pip install -U --force-reinstall pillow-simd
 
-#pip install git+https://github.com/EasternJournalist/utils3d.git@9a4eb15e4021b67b12c460c7057d642626897ec8 --no-cache-dir --no-build-isolation
+pip install git+https://github.com/EasternJournalist/utils3d.git@9a4eb15e4021b67b12c460c7057d642626897ec8 --no-cache-dir --no-build-isolation
 
 pip install xformers==0.0.29.post3 \
   --index-url https://download.pytorch.org/whl/cu124
@@ -37,9 +37,6 @@ cd flash-attention
 python setup.py install
 
 cd ../nvdiffrast
-python setup.py install
-
-cd ../diffoctreerast
 python setup.py install
 
 cd ../diffoctreerast
